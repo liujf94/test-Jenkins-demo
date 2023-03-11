3e92606d62c6ea55bc72fb7b782e6a54ac7a9e47
liujiangfeng <liujf_email@163.com>
2023-03-11 11:53:50 +0800

    提交代码2

--- src/main/java/org/example/ApiApplication.java
+++ src/main/java/org/example/ApiApplication.java
@@ -1,19 +1,17 @@
 package org.example;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * <p>create at 2020/7/16</p>
- *
- * @author liufl
- * @since 1.0
- */
+
 @SpringBootApplication
-public class HujingMcApiApplication {
+@Slf4j
+public class ApiApplication {
     public static void main(@NotNull String[] args) {
-        SpringApplication.run(HujingMcApiApplication.class, args);
+        SpringApplication.run(ApiApplication.class, args);
+        log.info("程序已启动");
     }
 }
--- src/main/java/org/example/controller/HelloController.java
+++ src/main/java/org/example/controller/HelloController.java
@@ -1,15 +1,18 @@
 package org.example.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
 @RestController
+@Slf4j
 public class HelloController {
 
     @RequestMapping("/hello")
     public String hello(){
-        return "hello "+new Date();
+        log.info("调用hello方法");
+        return "你好啊 "+new Date();
     }
 }
